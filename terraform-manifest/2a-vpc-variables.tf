36ac29f8da07f5126e3338a817f66dea963b4abb
Nnanna Victor Odeghe <odeghenn@gmail.com>
2024-04-06 02:16:36 +0100

    ....

--- terraform-manifest/2a-vpc-variables.tf
+++ terraform-manifest/2a-vpc-variables.tf
@@ -18,28 +18,28 @@ variable "vpc_cidr_block" {
 variable "vpc_availability_zones" {
   description = "VPC Availability Zones"
   type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-1a"]
 }
 
 # VPC Public Subnets
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  default = ["10.0.101.0/24"]
 }
 
 # VPC Private Subnets
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24"]
 }
 
 # VPC Database Subnets
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+  default = ["10.0.151.0/24"]
 }
 
 # VPC Create Database Subnet Group (True / False)
